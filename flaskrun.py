6adaa0a92f419cb6f3c0b6521b06383241a8cb91
Jeff Yang <jho.yang96@gmail.com>
2019-11-03 23:31:30 -0600

    added timestamp

--- flaskrun.py
+++ flaskrun.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 import sentence, os, tweet_generator
+import datetime
+ 
 app = Flask(__name__)
+timestamp = None 
 
 @app.route('/')
 def main():
-    return render_template('index.html', sentence=sentence.run())
+    global timestamp
+    return render_template('index.html', sentence=sentence.run(), timestamp=timestamp)
 
 @app.route('/tweet', methods=['POST', 'GET'])
 def tweet():
     status = request.form['status']
     tweet_generator.tweet(status)
+    currentDT = datetime.datetime.now()
+    global timestamp
+    timestamp = currentDT.strftime("%Y-%m-%d, %H:%M")
     return redirect('/')
 
 if __name__ == '__main__':
