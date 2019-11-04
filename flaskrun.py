0fe2c4b6933842c2bf16effb92709911bd27b90f
Jeff Yang <jho.yang96@gmail.com>
2019-11-04 00:58:35 -0600

    added tweet timestamp

--- flaskrun.py
+++ flaskrun.py
@@ -1,22 +1,17 @@
 from flask import Flask, render_template, request, redirect
 import sentence, os, tweet_generator
-import datetime
  
 app = Flask(__name__)
-timestamp = None 
 
 @app.route('/')
 def main():
-    global timestamp
-    return render_template('index.html', sentence=sentence.run(), timestamp=timestamp)
+    last_tweet_at = tweet_generator.fetch_timestamp()
+    return render_template('index.html', sentence=sentence.run(), timestamp=last_tweet_at)
 
 @app.route('/tweet', methods=['POST', 'GET'])
 def tweet():
     status = request.form['status']
     tweet_generator.tweet(status)
-    currentDT = datetime.datetime.now()
-    global timestamp
-    timestamp = currentDT.strftime("%Y-%m-%d, %H:%M")
     return redirect('/')
 
 if __name__ == '__main__':
--- tweet_generator.py
+++ tweet_generator.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
-import tweepy, os
+import tweepy, os, pytz, datetime, time
+from tzlocal import get_localzone
 
 load_dotenv()
 
@@ -9,13 +10,23 @@ ACCESS_KEY = os.getenv("ACCESS_KEY")
 ACCESS_SECRET_KEY = os.getenv("ACCESS_SECRET_KEY")
 
 def tweet(status):
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
-	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET_KEY)
-	api = tweepy.API(auth)
-	api.update_status(status)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET_KEY)
+    api = tweepy.API(auth)
+    api.update_status(status)
 
-	return status
+    return status
 
+def fetch_timestamp():
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET_KEY)
+    api = tweepy.API(auth)
+    last_tweet = api.user_timeline(id = api.me().id, count = 1)[0]
+    timestamp = last_tweet.created_at #returns datetime.datetime object
+    local_timezone = get_localzone()
+    smart_timestamp = timestamp.replace(tzinfo=pytz.utc).astimezone(local_timezone).strftime('%Y-%m-%d, %H:%M')
+    
+    return smart_timestamp
 
 
 
